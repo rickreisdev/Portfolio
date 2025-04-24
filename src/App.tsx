e45f81a9bb6d22c79dd806005bfb8ba1ffa84034
Rick <ricleyreis@gmail.com>
2025-04-23 22:15:43 -0300

    SEO: tornando titulo de descrição do site dinamico por tradução

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,30 @@ import Contato from "./sessions/Contato.tsx";
 import Footer from "./sessions/Footer.tsx";
 import Header from "./sessions/Header.tsx";
 import { Outlet } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet-async";
 
 function App() {
-  return (
-    <>
-      <div className="page-container">
-        <Header />
-        <BtnTopo />
-        <Outlet/>
-        <Contato />
-        <Footer />
-      </div>
-    </>
-  );
+    const { t } = useTranslation();
+    return (
+        <>
+            <Helmet>
+                <title>{t("tituloHome")}</title>
+                <meta
+                    name="description"
+                    content={t("descHome")}
+                />
+            </Helmet>
+
+            <div className="page-container">
+                <Header />
+                <BtnTopo />
+                <Outlet />
+                <Contato />
+                <Footer />
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,58 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import './lib/i18n'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+    Route,
+    Routes,
+    BrowserRouter,
+} from "react-router-dom";
+import "./lib/i18n";
 
-import "./assets/styles/global.scss"
-import "./assets/styles/media-query/query1085px.scss"
-import "./assets/styles/media-query/query-375px.scss"
-import "./assets/styles/media-query/query-400px.scss"
-import "./assets/styles/media-query/query-425px.scss"
-import "./assets/styles/media-query/query485px.scss"
-import "./assets/styles/media-query/query710px.scss"
-import "./assets/styles/media-query/query2560px.scss"
-import "./assets/styles/media-query/query1343-1085px.scss"
-import "./assets/styles/media-query/query-1343minpx.scss"
+import "./assets/styles/global.scss";
+import "./assets/styles/media-query/query1085px.scss";
+import "./assets/styles/media-query/query-375px.scss";
+import "./assets/styles/media-query/query-400px.scss";
+import "./assets/styles/media-query/query-425px.scss";
+import "./assets/styles/media-query/query485px.scss";
+import "./assets/styles/media-query/query710px.scss";
+import "./assets/styles/media-query/query2560px.scss";
+import "./assets/styles/media-query/query1343-1085px.scss";
+import "./assets/styles/media-query/query-1343minpx.scss";
 // import Experiencia from './sessions/Experiencia';
-import Home from './sessions/Home';
-import Sobre from './sessions/Sobre';
-import Habilidades from './sessions/Habilidades';
-import Projetos from './sessions/Projetos';
+import { HelmetProvider } from "react-helmet-async";
+import Home from "./sessions/Home";
+import Sobre from "./sessions/Sobre";
+import Habilidades from "./sessions/Habilidades";
+import Projetos from "./sessions/Projetos";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path='/' element={<Home/>} />
-          <Route path='/sobre' element={<Sobre/>} />
-          {/* <Route path='/experiencia' element={<Experiencia />} /> */}
-          <Route path='/habilidades' element={<Habilidades/>} />
-          <Route path='/projetos' element={<Projetos/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(
+    document.getElementById("root")!
+).render(
+    <React.StrictMode>
+        <HelmetProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />}>
+                        <Route
+                            path="/"
+                            element={<Home />}
+                        />
+                        <Route
+                            path="/sobre"
+                            element={<Sobre />}
+                        />
+                        {/* <Route path='/experiencia' element={<Experiencia />} /> */}
+                        <Route
+                            path="/habilidades"
+                            element={<Habilidades />}
+                        />
+                        <Route
+                            path="/projetos"
+                            element={<Projetos />}
+                        />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </HelmetProvider>
+    </React.StrictMode>
+);
