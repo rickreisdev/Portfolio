81dc2b107cdeb9b0ac2240ce75cdd48cabe2c09b
Rick <ricleyreis@gmail.com>
2025-09-26 20:03:33 -0300

    refactor: tornando título e descrição do site dinâmicos de acordo com a rota acessada

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,38 @@ import BtnTopo from "./components/BtnTopo.tsx";
 import Contato from "./sessions/Contato.tsx";
 import Footer from "./components/Footer.tsx";
 import Header from "./components/Header.tsx";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Helmet } from "react-helmet-async";
 
 function App() {
     const { t } = useTranslation();
+    const location = useLocation();
+    const { pathname } = location;
+
+    const rotaAudiovisual =
+        pathname.startsWith("/audiovisual");
+    const rotaDev = pathname.startsWith("/dev");
+
     return (
         <>
             <Helmet>
-                <title>{t("tituloHome")}</title>
+                <title>
+                    {rotaDev
+                        ? t("tituloHomeDev")
+                        : rotaAudiovisual
+                        ? t("tituloHomeAudiovisual")
+                        : t("tituloHome")}
+                </title>
                 <meta
                     name="description"
-                    content={t("descHome")}
+                    content={
+                        rotaDev
+                            ? t("descHomeDev")
+                            : rotaAudiovisual
+                            ? t("descHomeAudiovisual")
+                            : t("descHome")
+                    }
                 />
             </Helmet>
 
