fb76a2fdfeff22c5c03e7b692ac29f22fc51d2ed
Rick <ricleyreis@gmail.com>
2025-10-23 16:02:00 -0300

    feat: ajustando acessibilidade

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC = () => {
                     <div className="copyright">
                         <span className="year">{currentYear}</span>
                         <span className="brand-name">
-                            <img src={logo} alt="logo RickReis" />
+                            <img src={logo} alt="RickReis" />
                         </span>
                     </div>
                 </div>
